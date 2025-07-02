912652e8edcff7bddb6a9f9811134db2423c6ec2
hooved <172129504+hooved@users.noreply.github.com>
2025-07-02 13:56:29 -0400

    make training loop work with cpu

--- stable_diffusion/main.py
+++ stable_diffusion/main.py
@@ -308,8 +308,8 @@ class CUDACallback(Callback):
 
     def on_train_epoch_start(self, trainer, pl_module):
         # Reset the memory use counter
-        torch.cuda.reset_peak_memory_stats(trainer.strategy.root_device.index)
-        torch.cuda.synchronize(trainer.strategy.root_device.index)
+        #torch.cuda.reset_peak_memory_stats(trainer.strategy.root_device.index)
+        #torch.cuda.synchronize(trainer.strategy.root_device.index)
         self.start_time = time.time()
 
     def on_train_epoch_end(self, trainer, pl_module):
