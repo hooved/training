17bd0e34352d68e19535c898e135dd0653c7bea5
hooved <172129504+hooved@users.noreply.github.com>
2025-07-25 10:26:13 -0400

    checkpoint fp16 forward

--- stable_diffusion/ldm/modules/diffusionmodules/openaimodel.py
+++ stable_diffusion/ldm/modules/diffusionmodules/openaimodel.py
@@ -775,7 +775,6 @@ class UNetModel(nn.Module):
             # x: (1,4,64,64) float32, timesteps: (1,) int64, context: (1,77,1024) float32
             # inference/validation: B doubled for uncond/cond
 
-            """
             # capture model init values
             state_dict = self.state_dict()
             for k,v in state_dict.items():
@@ -784,8 +783,8 @@ class UNetModel(nn.Module):
                 state_dict[k] = v.contiguous()
             save_file(state_dict, "checkpoints/unet_training_init_model.safetensors")
             #globvars.export_tensors.update({"x": x.cpu(), "timesteps": timesteps.cpu(), "context": context.cpu()})
-            globvars.val.update({"x": x.cpu(), "timesteps": timesteps.cpu(), "context": context.cpu()})
-            """
+            globvars.mixed.update({"x": x.detach().cpu(), "timesteps": timesteps.detach().cpu(), "context": context.detach().cpu()})
+
             """
             state_dict = self.state_dict()
             for k,v in state_dict.items():
@@ -830,10 +829,11 @@ class UNetModel(nn.Module):
                 #self.out.to("cuda:0")
                 #ret = self.out(h)
 
-                #globvars.export_tensors["out"] = ret.cpu()
                 ###for i, block in enumerate(export_tensors['resblocks']):
                     ###for j, t in enumerate(block):
                         ###export_tensors[f'resblocks.{i}.{j}'] = t
                 ###del export_tensors['resblocks']
-                #save_file(globvars.export_tensors, "datasets/tensors/unet_training_io.safetensors")
+
+                globvars.mixed["ret"] = ret.detach().cpu()
+                save_file(globvars.mixed, "checkpoints/mixed.safetensors")
                 return ret # 16
